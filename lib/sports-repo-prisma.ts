ba6ad332fe16da680fe00c3570fd1d4c9f98955d
Sherif Awad <eng.sherifawad@gamil.com>
2023-05-25 00:32:43 +0200

    feat:add sport  repo prisma

---
+++ lib/sports-repo-prisma.ts
@@ -0,0 +1,88 @@
+import { prisma } from "lib/prisma";
+import type { Prisma, Sport, Penalty, Discount } from "@prisma/client";
+
+type SportToEditType = Sport & {
+    discounts?: Prisma.DiscountWhereUniqueInput[] | null;
+};
+
+export const sportsRepo = {
+    getSports: async () => await prisma.sport.findMany({}),
+    getAll: async () =>
+        await prisma.sport.findMany({
+            include: {
+                DiscountOptions: true,
+                Penalty: true,
+            },
+        }),
+    getById: async (id: number) =>
+        await prisma.sport.findUnique({ where: { id } }),
+    find: async (x: Prisma.SportWhereInput) =>
+        await prisma.sport.findFirst({ where: x }),
+    create,
+    update,
+    delete: _delete,
+};
+
+async function create(sport: SportToEditType) {
+    const { discounts, ...rest } = sport;
+    try {
+        return {
+            sport: await prisma.sport.create({
+                data: {
+                    ...rest,
+                    DiscountOptions: connectSportToDiscounts(discounts),
+                },
+            }),
+            error: null,
+        };
+    } catch (error) {
+        return { sport: null, error: error };
+    }
+}
+
+async function update(id: number, params: SportToEditType) {
+    const { discounts, ...rest } = params;
+
+    try {
+        return {
+            sport: await prisma.sport.update({
+                where: { id },
+                data: {
+                    ...rest,
+                    DiscountOptions: connectSportToDiscounts(discounts),
+                },
+            }),
+            error: null,
+        };
+    } catch (error) {
+        return { sport: null, error: error };
+    }
+}
+
+// prefixed with underscore '_' because 'delete' is a reserved word in javascript
+async function _delete(id: number) {
+    try {
+        return {
+            sport: await prisma.sport.delete({
+                where: { id },
+            }),
+            error: null,
+        };
+    } catch (error) {
+        return { sport: null, error: error };
+    }
+}
+
+function connectSportToDiscounts(
+    discounts: Prisma.DiscountWhereUniqueInput[] | undefined | null
+) {
+    if (!discounts || discounts == null || discounts.length < 1)
+        return undefined;
+    return {
+        connect: discounts.map((id) => ({
+            id,
+        })),
+    } as
+        | Prisma.DiscountUncheckedCreateNestedManyWithoutSportsInput
+        | Prisma.DiscountCreateNestedManyWithoutSportsInput;
+}
