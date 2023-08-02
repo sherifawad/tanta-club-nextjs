6a67cdaaedf142a4dc790ab163cb37f0d8489ddc
Sherif Awad <eng.sherifawad@gamil.com>
2023-08-02 17:49:15 +0200

    fix: change base url in production env

--- lib/utils.ts
+++ lib/utils.ts
@@ -52,7 +52,7 @@ export const arrayToReactSelectOption = (
 
 export const getBaseUrl = () => {
     if (process.env.NEXT_PUBLIC_VERCEL_ENV === "production")
-        return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+        return "https://clubsport.vercel.app";
     if (process.env.NEXT_PUBLIC_VERCEL_ENV === "preview")
         return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
     return "http://localhost:3000";
